9f97ca885ca787154078efe123ab255807e78580
Yosuf20 <jyosuf6@gmail.com>
2026-07-01 20:34:53 +0530

    Fixed chain fucntion call

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 import streamlit as st
 import random
 import time
-from chatbot import get_response
+from chatbot import load_chain, get_response
 
-import chatbot
-print(dir(chatbot))
 
 st.title("Youtube Chatbot")
 
+url = st.sidebar.text_input("Enter Youtube Video Link", type="default")
 
-def response_generator(ques):
-    response = get_response(ques)
+if url:
+    if st.session_state.get("loaded_url") != url:
+        chain = load_chain(url)
+        if chain == None:
+            print("No Transcript Found")
+        else:
+            st.session_state.loaded_url = url
+            st.session_state.message = []
+            st.session_state.chain = chain
+            st.sidebar.success("Ready!")
+
+
+
+def response_generator(ques, chain):
+    response = get_response(ques, chain)
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
@@ -29,19 +41,19 @@ for message in st.session_state.messages:
 prompt = st.chat_input("Say Something")
 
 if prompt:
-    
+
     with st.chat_message("User"):
         st.markdown(prompt)
         st.session_state.messages.append({"role" : "user", "content" : prompt})
 
     with st.chat_message("assistant"):
-        res = st.write_stream(response_generator(prompt))
+        res = st.write_stream(response_generator(prompt, st.session_state.chain))
         st.session_state.messages.append({"role" : "assistant" , "content" : res})
 
 
 # Side bar
 API_KEY = st.sidebar.text_input("API KEY", type="password")
-url = st.sidebar.text_input("Enter Youtube Video Link", type="default")
+
 
 
 
